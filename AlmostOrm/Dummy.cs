9000c1f5dc55b48ade769577e8544ed17ab803ab
TwiWeYo <portportpor@gmail.com>
2024-04-30 00:00:28 +0300

    feat: initial commit

---
+++ AlmostOrm/Dummy.cs
@@ -0,0 +1,11 @@
+﻿namespace AlmostOrm
+{
+    public class Dummy
+    {
+        public string A { get; set; }
+        public int B { get; set; }
+        public float C { get; set; }
+        public double D { get; set; }
+        public decimal E { get; set; }
+    }
+}
---
+++ AlmostOrm/SqlClassMap.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace AlmostOrm
+{
+    public static class SqlClassMap
+    {
+        private static readonly Type _typeInt = typeof(int);
+        private static readonly Type _typeLong = typeof(long);
+        private static readonly Type _typeFloat = typeof(float);
+        private static readonly Type _typeDouble = typeof(double);
+        private static readonly Type _typeDecimal = typeof(decimal);
+        private static readonly Type _typeString = typeof(string);
+        public static void GenerateTestMap<T>() where T : class
+        {
+            var type = typeof(T);
+
+            var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var sb = new StringBuilder();
+            sb.Append(string.Join(",\n", GetMappedStrings(properties)));
+
+            Console.WriteLine(sb.ToString());
+        }
+
+        private static IEnumerable<string> GetMappedStrings(PropertyInfo[] properties)
+        {
+            foreach (var property in properties)
+            {
+                var propType = property.PropertyType;
+                var name = property.Name;
+
+                var sqlType = propType switch
+                {
+                    _ when propType == _typeInt => "int",
+                    _ when propType == _typeLong => "bigint",
+                    _ when propType == _typeFloat || propType == _typeDouble || propType == _typeDecimal => "numeric(12, 6)",
+                    _ when propType == _typeString => "varchar(128)",
+                    _ => throw new NotSupportedException($"{propType.Name} is not yet supported")
+                };
+
+                yield return $"{name} {sqlType}";
+            }
+        }
+    }
+}
