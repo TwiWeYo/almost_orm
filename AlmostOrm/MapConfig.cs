9bd04fa831550253056fea00592c9ddafdf19a41
TwiWeYo <portportpor@gmail.com>
2024-05-01 19:11:56 +0300

    added nulable type support and basic configuration

---
+++ AlmostOrm/MapConfig.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq.Expressions;
+
+namespace AlmostOrm.MapConfig
+{
+    public enum OnConflict
+    {
+        Ignore,
+        Nothing,
+        Update
+    }
+    public class MapConfig<T> where T : class
+    {
+        public string TableName { get; private set; }
+        public bool HasId { get; private set; }
+        public OnConflict OnConflict { get; private set; }
+        public bool IsUniqueIndex { get; private set; }
+        public Expression<Func<T, object>>[] Index { get; private set; }
+
+        public MapConfig<T> WithTableName(string tableName)
+        {
+            TableName = tableName;
+            return this;
+        }
+
+        public MapConfig<T> WithId()
+        {
+            HasId = true;
+            return this;
+        }
+
+        public MapConfig<T> OnConflictDo(OnConflict onConflict)
+        {
+            OnConflict = onConflict;
+            return this;
+        }
+
+        public MapConfig<T> WithIndex(bool isUnique, params Expression<Func<T, object>>[] selector)
+        {
+            IsUniqueIndex = isUnique;
+            Index = selector;
+            return this;
+        }
+    }
+}
--- AlmostOrm/SqlClassMap.cs
+++ AlmostOrm/SqlClassMap.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using AlmostOrm.MapConfig;
+using System.Reflection;
 using System.Text;
 
 namespace AlmostOrm
@@ -11,23 +12,39 @@ namespace AlmostOrm
         private static readonly Type _typeDouble = typeof(double);
         private static readonly Type _typeDecimal = typeof(decimal);
         private static readonly Type _typeString = typeof(string);
-        public static void GenerateTestMap<T>() where T : class
+        public static void GenerateTestMap<T>(MapConfig<T>? config = null) where T : class
         {
+            var sb = new StringBuilder();
+            sb.Append(@$"CREATE TABLE {config?.TableName ?? nameof(T).ToLower()}
+(
+");
             var type = typeof(T);
 
             var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
-            var sb = new StringBuilder();
             sb.Append(string.Join(",\n", GetMappedStrings(properties)));
+            sb.AppendLine("\n);");
 
             Console.WriteLine(sb.ToString());
         }
 
         private static IEnumerable<string> GetMappedStrings(PropertyInfo[] properties)
         {
+            var nullabilityInfoContext = new NullabilityInfoContext();
+
             foreach (var property in properties)
             {
                 var propType = property.PropertyType;
+                var nullabilityInfo = nullabilityInfoContext.Create(property);
+
                 var name = property.Name;
+                var additionalInfo = " NOT NULL";
+
+                if (nullabilityInfo.WriteState is NullabilityState.Nullable)
+                {
+                    propType = propType.IsValueType ? Nullable.GetUnderlyingType(propType) : propType;
+
+                    additionalInfo = string.Empty;
+                }
 
                 var sqlType = propType switch
                 {
@@ -35,10 +52,10 @@ namespace AlmostOrm
                     _ when propType == _typeLong => "bigint",
                     _ when propType == _typeFloat || propType == _typeDouble || propType == _typeDecimal => "numeric(12, 6)",
                     _ when propType == _typeString => "varchar(128)",
-                    _ => throw new NotSupportedException($"{propType.Name} is not yet supported")
+                    _ => throw new NotSupportedException($"{propType!.Name} is not yet supported")
                 };
 
-                yield return $"{name} {sqlType}";
+                yield return $"    {name} {sqlType}{additionalInfo}";
             }
         }
     }
