a01c92dcdda980731d87d4f50d60b458bcd34119
TwiWeYo <portportpor@gmail.com>
2024-06-20 22:35:18 +0300

    allow nullable for settings

--- AlmostOrm/Settings.cs
+++ AlmostOrm/Settings.cs
@@ -6,13 +6,13 @@ namespace AlmostOrm
     {
         private string? tableTemplate;
         private string? procedureTemplate;
-        [JsonIgnore] public string TableTemplate => tableTemplate ??= GetTemplate(TableTemplatePath);
-        public string TableTemplatePath { get; set; }
-        [JsonIgnore] public string ProcedureTemplate => procedureTemplate ??= GetTemplate(ProcedureTemplatePath);
-        public string ProcedureTemplatePath { get; set; }
-        public Dictionary<string, string> TypeMaps { get; set; }
-        public string IndexTemplate { get; set; }
-        public string IdTemplate { get; set; }
+        [JsonIgnore] public string TableTemplate => tableTemplate ??= GetTemplate(TableTemplatePath!);
+        public string? TableTemplatePath { get; set; }
+        [JsonIgnore] public string ProcedureTemplate => procedureTemplate ??= GetTemplate(ProcedureTemplatePath!);
+        public string? ProcedureTemplatePath { get; set; }
+        public Dictionary<string, string>? TypeMaps { get; set; }
+        public string? IndexTemplate { get; set; }
+        public string? IdTemplate { get; set; }
 
         private string GetTemplate(string path)
         {
